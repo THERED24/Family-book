9144e4fe3f1d04f7a72a671a7a728b7f980312f9
THERED24 <72960428+THERED24@users.noreply.github.com>
2021-08-13 16:54:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+var pictures=["grandpa.jpg","granny.jpg","Lewis.webp","SLENDRINA.jpg","SLENDER MAN.jpg","SPIDER BABY.png"];
+var names = ["Granpa","Granny","me","Slendrina","Slender man","Spider baby"];
